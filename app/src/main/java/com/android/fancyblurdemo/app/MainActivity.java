3c852b07cec58f99f18917fdee108aa281a2dbe3
Kevin Marlow <kevin.marlow@ymedialabs.com>
2014-04-09 18:02:13 -0700

    Adjusted Interpolator and DayDream name.

--- app/src/main/java/com/android/fancyblurdemo/app/MainActivity.java
+++ app/src/main/java/com/android/fancyblurdemo/app/MainActivity.java
@@ -23,7 +23,6 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -142,7 +141,6 @@ public class MainActivity extends FragmentActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
         mViewPager.setOnPageChangeListener(mPageChangeListener);
         mViewPager.setScrollDurationFactor(sUseHighRes ? 3 : 2);
-        mViewPager.setInterpolator(new AccelerateDecelerateInterpolator());
 
         mViewPager.setOnTouchListener(new View.OnTouchListener() {
             @Override
